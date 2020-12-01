494cf069828e749961d42b7ad884ce7d206de5a0
Sumit Goyal <sgoyal@americanfirstfinance.com>
2020-11-30 18:55:15 -0600

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+G
+Python_on_openshift.iml,3\4\34f0ad32d30c8d1a8f98f63be1b22a7fc7695de1
+B
+hello_world_api.py,0\8\083666098d783bf09d88e3de8f6766ef4ed32645
---
+++ hello_world_api.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+"""
+every flask must have a unique name in the project
+"""
+
+app = Flask(__name__)  ## __name__  > is always unique for the project
+
+
+@app.route('/')
+def home_page():
+    return "hello word"
+    #return render_template("homepage.html", posts=posts)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  ##more info in case of error 
